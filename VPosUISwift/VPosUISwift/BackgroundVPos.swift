fe6286611293e8f2abdd3954f65ad17139ebf0b3
ChuTrungKien <chutrungkien11997@gmail.com>
2024-04-08 17:33:45 +0700

    kien committtt

--- VPosUISwift/VPosUISwift/BackgroundVPos.swift
+++ VPosUISwift/VPosUISwift/BackgroundVPos.swift
@@ -26,24 +26,72 @@ struct BackgroundVPos: View {
             
             Spacer()
             
-            ScrollView {
+            ScrollView(showsIndicators: false) {
                 VStack {
                     ForEach(1...20, id: \.self) { index in
                         Text("Item \(index)")
                             .padding()
                             .frame(width: UIScreen.SCREEN_W - 10)
                     }
+                    MoTaGiayPhep()
+
                 }
             }
-            .frame(height: UIScreen.SCREEN_H - 54)
             .background(Color.white)
         }
         .background(Color.gray)
     }
 }
 
+struct MoTaGiayPhep: View {
+    var body: some View {
+        VStack(alignment: .center) {
+            Text("Công ty cổ phần dịch vụ nền di động Việt Nam")
+                .font(.footnote)
+            Text("Giấy phép trung gian thanh toán 41/GP - NHNN 12/03/2018")
+                .font(.footnote)
+                .padding(.top, 1)
+            HStack(alignment: .center) {
+                Text("VPos")
+                    .font(.largeTitle)
+                Text("phiên bản 1.1")
+                    .font(.title2)
+                    .padding(.leading, 10)
+            }
+            .frame(width: UIScreen.SCREEN_W - 10, height: 44)
+            Text("")
+                .frame(height: 50)
+        }
+    }
+}
+
+struct NhomDangKy: View {
+    var body: some View {
+        VStack(alignment: .center) {
+            HStack(alignment: .center) {
+                Text("Pass")
+            }
+            Divider()
+            HStack(alignment: .center) {
+                Text("Pass token")
+            }
+            HStack {
+                Button(action: {
+                    //action here
+                }, label: {
+                    Text("Đăng ký")
+                        .padding()
+                        .foregroundColor(.white)
+                        .background(.blue)
+                        .cornerRadius(12)
+                })
+            }
+        }
+    }
+}
+
 #Preview {
-    BackgroundVPos()
+    NhomDangKy()
 }
 
 extension UIScreen {
