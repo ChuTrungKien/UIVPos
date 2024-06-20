647d9c2db74e6401490fa3902c87e888714093dd
ChuTrungKien <chutrungkien11997@gmail.com>
2024-06-20 10:45:58 +0700

    commit them file mo rong

---
+++ VPosUISwift/VPosUISwift/ExtentFile.swift
@@ -0,0 +1,27 @@
+//
+//  ExtentFile.swift
+//  VPosUISwift
+//
+//  Created by QRVN on 20/06/2024.
+//
+
+import Foundation
+import SwiftUI
+
+struct SafeAreaInsetsModifier: ViewModifier {
+    let action: (EdgeInsets) -> Void
+    func body(content: Content) -> some View {
+        GeometryReader { geometry in
+            Color.clear
+                .onAppear {
+                    action(geometry.safeAreaInsets)
+                }
+        }
+    }
+}
+
+extension View {
+    func onSafeAreaInsets(perfom action: @escaping (EdgeInsets) -> Void) -> some View {
+        self.modifier(SafeAreaInsetsModifier(action: action))
+    }
+}
