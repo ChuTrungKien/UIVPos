593ca93f5f79b32a57ed90a1db7a7ade9f771326
ChuTrungKien <chutrungkien11997@gmail.com>
2024-07-16 17:54:16 +0700

    commm

--- VPosUISwift/VPosUISwift/Test/BaseViewController.swift
+++ VPosUISwift/VPosUISwift/Test/BaseViewController.swift
@@ -6,3 +6,92 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct BaseViewController<Content: View>: View {
+    let title: String
+    let leadingItem: AnyView?
+    let trailingItem: AnyView?
+    let content: Content
+    
+    init(title: String,
+         leadingItem: AnyView?  = AnyView(Button(action: {
+        
+    }) {
+        Text("Back")
+            .foregroundColor(.blue)
+    }),
+         trailingItem: AnyView?,
+         @ViewBuilder content: () -> Content) {
+        self.title = title
+        self.content = content()
+        self.leadingItem = leadingItem
+        self.trailingItem = trailingItem
+    }
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                content
+            }
+            .navigationBarTitle(Text(title), displayMode: .inline)
+            .navigationBarItems(leading: leadingItem, trailing: trailingItem)
+        }
+    }
+}
+
+struct ViewControllerA: View {
+    var body: some View {
+        BaseViewController(title: "ViewController A",
+                           trailingItem: AnyView(
+                            Button(action: {
+                                print("Home button tapped")
+                            }) {
+                                Text("Home")
+                                    .foregroundColor(.blue)
+                            }
+                           )) {
+                               List {
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                                   Text("First row")
+                               }
+        }
+    }
+}
+
+#Preview {
+    ViewControllerA()
+}
